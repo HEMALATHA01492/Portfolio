e1d89e635b506341ec4d99cbd15b421f0b3b0dcf
HEMALATHA01492 <hema01492@gmail.com>
2024-01-11 11:08:13 +0530

    portfolio-react

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import styles from "./contact.module.css";
 import { getImageUrl } from "../../utils";
-import Footer from "../footer/Footer";
-import {FaLinkedin} from 'react-icons/fa';
 import { ImMobile } from "react-icons/im";
 import { MdEmail } from "react-icons/md";
-
 
 const Contact = () => {
   return (
@@ -14,13 +11,11 @@ const Contact = () => {
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
 
-
         <div>
           <a href="mailto:hema01492@gmail.com" target="_blank">
             <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
           </a>
           <a href="https://www.linkedin.com/in/hema01492" target="_blank">
-
             <img
               src={getImageUrl("contact/linkedinIcon.png")}
               alt="LinkedIn icon"
@@ -32,16 +27,18 @@ const Contact = () => {
               alt="Github icon"
             />
           </a>
-          
-      </div>
-      
+        </div>
       </div>
       <div className={styles.mainLink}>
-      <ul className={styles.links}>
-      <li className={styles.link}>
-        <ImMobile size={29}/> 9962998160</li>
-      <li><MdEmail size={29} className={styles.emailImg}/><span className={styles.email}>hema01492@gmail.com</span></li>
-      </ul>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <ImMobile size={29} /> 9962998160
+          </li>
+          <li>
+            <MdEmail size={29} className={styles.emailImg} />
+            <span className={styles.email}>hema01492@gmail.com</span>
+          </li>
+        </ul>
       </div>
     </footer>
   );
